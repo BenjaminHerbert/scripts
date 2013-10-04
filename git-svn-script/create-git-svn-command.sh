3204b49deafc6f4c024b14c5198804fbe7e3e821
Benjamin Herbert <bherbert@trustinternational.com>
2013-10-04 16:01:51 +0200

    added script to generate a git svn command which reads the 100 latest revisions into the history

---
+++ git-svn-script/create-git-svn-command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+numChanges=100
+latest_rev=$(svn info $1 | grep Revision: | sed -e's/Revision: //')
+rev_start=$(echo $latest_rev-$numChanges | bc)
+echo
+echo "This command will git svn clone your latest $numChanges changes"
+echo
+echo "git svn clone -r$rev_start:$latest_rev $1"
+echo
+
+
+
